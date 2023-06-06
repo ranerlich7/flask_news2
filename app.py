28bda475b849dc93ee79ed741c081c16c0212e7e
narnavg <narnavg@gmail.com>
2023-06-06 21:25:36 +0300

    added delete article

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 # init database 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # articles = ["This is todays news", "this is world news", "this is sports"]
 @app.route("/")
 def news():
-   res = cur.execute("SELECT * FROM article WHERE Category='News'")
+   res = cur.execute("SELECT *,rowid FROM article WHERE Category='News'")
    articles = res.fetchall()
    print(f"****{articles} ")
    return render_template("article.html", news=articles)
@@ -21,7 +21,7 @@ def news():
 # sport_articles = ["City won the premier leage", "Nadal loses a game", "Liverpool rocks"]
 @app.route("/sports")
 def sports():
-   res = cur.execute("SELECT * FROM article WHERE Category='Sports'")
+   res = cur.execute("SELECT *,rowid FROM article WHERE Category='Sports'")
    articles = res.fetchall()
    print(f"****{articles} ")
    return render_template("article.html", news=articles)
@@ -39,6 +39,13 @@ def addarticle():
         return "ADDED AN ARTICLE WOOHOO!"
     else:
         return render_template("add_article.html")
+
+@app.route("/delete_article/<id>", methods=["POST","GET"])
+def delete_article(id):
+    print(f"ID IS:{id}")
+    res = cur.execute(f"DELETE FROM article WHERE rowid={id};")
+    con.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
    app.run(debug=True, port=9000)

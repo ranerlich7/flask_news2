bb41b22797c379278342e7b9c83a7bc023b369eb
ran <ran@goz.com>
2023-06-13 12:36:27 +0300

    added api app, moved old app to full_app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,89 +15,83 @@ class Article(db.Model):
     image = db.Column(db.String(100))
     category = db.Column(db.String(50), nullable=False)
 
-@app.route("/")
-@app.route("/<cat>")
-def index(cat=None):
-    print(f'** {cat}')
-    if not cat:
+@app.route('/article')
+@app.route('/article/<id>')
+def index(id=None):
+    if not id:
         articles = Article.query.all()
     else:
-        # cat = f'%{cat}%'
-        articles = Article.query.filter(Article.category.ilike(cat)).all()
+        articles = [Article.query.get(id)]
     
+    a_list = []
+    for article in articles:
+        a_list.append({
+            "id": article.id,
+            "title": article.title,
+            "content": article.content,
+            "image": article.image,
+            "category": article.category
+        })
+    return jsonify(a_list if not id else a_list[0])
 
-    return render_template("index.html", news=articles)
 
 
-# @app.route("/article")
-@app.route("/article/<category>")
-def article(category):
-    articles = Article.query.filter(Article.category.ilike(category)).all()
-    print("articles", articles)
-    print(f"****{articles} ")
-    return render_template("article.html", news=articles)
-
-
-@app.route("/add_article", methods=["POST", "GET"])
-@app.route("/add_article/<id>", methods=["POST", "GET"])
+@app.route("/add_article", methods=["POST"])
+@app.route("/add_article/<id>", methods=["PUT"])
 def add_article(id=-1):
-    if request.method == "POST":
-        title = request.form.get("title")
-        content = request.form.get("content")
-        image_url = request.form.get("image-url")
-        category = request.form.get("category")
-        print(f"!!! {title},{content},{image_url},{category}")
-        # add new article
-        if id == -1:
-            article = Article(title=title, content=content,
-                              image=image_url, category=category)
-            db.session.add(article)
-            db.session.commit()
-        else:
-            article = Article.query.get(id)
-            article.title = title
+    article_json = request.get_json()
+    print("** got json:{article_json}")
+    title = article_json.get("title")
+    content = article_json.get("content")
+    image_url = article_json.get("image")
+    category = article_json.get("category")
+    
+    print(f"!!! {title},{content},{image_url},{category}")
+    # add new article
+    if id == -1:
+        article = Article(title=title, content=content,
+                            image=image_url, category=category)
+        db.session.add(article)
+        db.session.commit()
+        response_data = {'message': 'Resource created successfully'}
+        code = 201
+    else:
+        article = Article.query.get(id)
+        article.title = title
+        article.image = image_url
+        article.category = category
+        article.content = content
+        db.session.commit()
+        response_data = {'message': 'Updated successfully'}
+        code = 200
 
-            db.session.commit()
+ 
+    # Create a Flask response with the JSON data and 201 status code
+    response = make_response(jsonify(response_data), code)
 
-        return redirect(f'/article/{category}')
+    # Set the Content-Type header to application/json
+    response.headers['Content-Type'] = 'application/json'
 
-    article = Article.query.get(id)
-
-    return render_template("add_article.html", article=article)
+    return response
 
 
-@app.route("/delete_article/<id>", methods=["POST"])
+@app.route("/delete_article/<id>", methods=["DELETE"])
 def delete_article(id):
     article = Article.query.get(id)
     if article:
         db.session.delete(article)
         db.session.commit()
-        return redirect("/article/"+article.category)
+        response_data = {'message': f'Delete successful for resource {id}'}
+        response = make_response(jsonify(response_data), 204)
+        # Set the Content-Type header to application/json
+        response.headers['Content-Type'] = 'application/json'
+        return response    
     else:
         return 'Article not found.'
 
-# @app.route('/')
-# def index():
-#    article = Article(title='News 22222 is here',content="Hello this is content",image="https://picsum.photos/id/137/300/200", category="News")
-#    db.session.add(article)
-#    article = Article(title='News 33333 is here',content="Hello this is content",image="https://picsum.photos/id/137/300/200", category="News")
-#    db.session.add(article)
-#    db.session.commit()
-
-
-#    # Retrieve all users
-#    articles = Article.query.all()
-
-
-#    # Display users
-#    output = ''
-#    for article in articles:
-#        output += f'Article: {article.title}<br>'
-#    return output
-
-
 with app.app_context():
     db.create_all()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
